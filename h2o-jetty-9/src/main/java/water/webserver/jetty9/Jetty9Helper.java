a4b53c6a090bf3f19a1f3b13702fe5cb8c4b7cc7
Jan Sterba <jan.sterba@h2o.ai>
2018-12-06 12:51:21 +0100

    [HEXDEV-717] enable SPNEGO authentication
    - back-end part

--- h2o-jetty-9/src/main/java/water/webserver/jetty9/Jetty9Helper.java
+++ h2o-jetty-9/src/main/java/water/webserver/jetty9/Jetty9Helper.java
@@ -1,15 +1,10 @@
 package water.webserver.jetty9;
 
 import org.eclipse.jetty.jaas.JAASLoginService;
-import org.eclipse.jetty.security.Authenticator;
-import org.eclipse.jetty.security.ConstraintMapping;
-import org.eclipse.jetty.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.security.DefaultIdentityService;
-import org.eclipse.jetty.security.HashLoginService;
-import org.eclipse.jetty.security.IdentityService;
-import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.security.*;
 import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.security.authentication.FormAuthenticator;
+import org.eclipse.jetty.security.authentication.SpnegoAuthenticator;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.HandlerWrapper;
@@ -93,14 +88,21 @@ class Jetty9Helper {
 
         // REFER TO http://www.eclipse.org/jetty/documentation/9.1.4.v20140401/embedded-examples.html#embedded-secured-hello-handler
         final LoginService loginService;
+        final Authenticator primaryAuthenticator;
         switch (config.loginType) {
             case HASH:
                 loginService = new HashLoginService("H2O", config.login_conf);
+                primaryAuthenticator = new BasicAuthenticator();
                 break;
             case LDAP:
             case KERBEROS:
             case PAM:
                 loginService = new JAASLoginService(config.loginType.jaasRealm);
+                primaryAuthenticator = new BasicAuthenticator();
+                break;
+            case SPNEGO:
+                loginService = new SpnegoLoginService(config.loginType.jaasRealm, config.spnego_properties);
+                primaryAuthenticator = new SpnegoAuthenticator();
                 break;
             default:
                 throw new UnsupportedOperationException(config.loginType + ""); // this can never happen
@@ -127,15 +129,13 @@ class Jetty9Helper {
         // Authentication / Authorization
         final Authenticator authenticator;
         if (config.form_auth) {
-            BasicAuthenticator basicAuthenticator = new BasicAuthenticator();
             FormAuthenticator formAuthenticator = new FormAuthenticator("/login", "/loginError", false);
-            authenticator = new Jetty9DelegatingAuthenticator(basicAuthenticator, formAuthenticator);
+            authenticator = new Jetty9DelegatingAuthenticator(primaryAuthenticator, formAuthenticator);
         } else {
-            authenticator = new BasicAuthenticator();
+            authenticator = primaryAuthenticator;
         }
         security.setLoginService(loginService);
         security.setAuthenticator(authenticator);
-
 
         final SessionHandler sessionHandler = new SessionHandler();
         if (config.session_timeout > 0) {
